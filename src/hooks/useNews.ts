7298ad8ee513daecf5806e6e451275ea87d6c7ab
yousaf wazir <umbrellaman612@gmail.com>
2023-03-07 14:27:12 +0000

    hook is good

--- src/hooks/useNews.ts
+++ src/hooks/useNews.ts
@@ -33,11 +33,43 @@ interface UseNewsResult {
   isError: Error | undefined;
 }
 
-const useNews = ({ searchQuery }: { searchQuery: string }): UseNewsResult => {
+const useNews = ({
+  searchQuery,
+  from,
+  sortBy,
+}: {
+  searchQuery: string;
+  from?: string;
+  sortBy?: "popularity" | "publishedAt" | "relevancy";
+}): UseNewsResult => {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
+  let sortOption = "";
+  if (
+    sortBy === "popularity" ||
+    sortBy === "publishedAt" ||
+    sortBy === "relevancy"
+  ) {
+    sortOption = sortBy;
+  } else {
+    sortOption = "popularity";
+  }
+
+  let fromDate = "";
+  if (from) {
+    const today = new Date();
+    const fromTime = new Date(from);
+    const diffTime = Math.abs(today.getTime() - fromTime.getTime());
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    if (diffDays <= 30) {
+      fromDate = from;
+    }
+  }
+
   const { data, error } = useSWR<News>(
-    `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=715bdbc7832b4791b9ef7d1be0b356eb`,
+    `https://newsapi.org/v2/everything?q=${searchQuery}${
+      fromDate ? `&from=${fromDate}` : ""
+    }&sortBy=${sortOption}&apiKey=715bdbc7832b4791b9ef7d1be0b356eb`,
     fetcher
   );
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+import useNews from "@/hooks/useNews";
+
 export default function Page() {
-  return <section className="overflow-auto h-full max-h-full">lorem1000</section>;
+  const { data } = useNews({ searchQuery: "apple", from:"2023-02-04" });
+  return (
+    <section className="overflow-auto h-full max-h-full">
+      {JSON.stringify(data)}
+    </section>
+  );
 }
